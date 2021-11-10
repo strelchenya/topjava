df5aab78ee3b0d258c1485a7b95ee3ab1d802a36
Igor Strelchenya <just.sicarius@gmail.com>
2021-11-10 23:42:18 +0300

    HW05: create template pattern

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/ConvertLocalDateTime.java
+++
@@ -1,8 +0,0 @@
-package ru.javawebinar.topjava.repository.jdbc;
-
-import java.time.LocalDateTime;
-
-
-public abstract class ConvertLocalDateTime {
-    public abstract <T> T convert(LocalDateTime localDateTime);
-}
--- src/main/java/ru/javawebinar/topjava/repository/jdbc/ConvertPostgresJdbc.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/ConvertPostgresJdbc.java
@@ -1,16 +1,39 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 @Profile(Profiles.POSTGRES_DB)
-public class ConvertPostgresJdbc extends ConvertLocalDateTime {
+public class ConvertPostgresJdbc extends JdbcMealRepository {
+
+    public ConvertPostgresJdbc(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
     @Override
-    public LocalDateTime convert(LocalDateTime localDateTime) {
-        return localDateTime;
+    public MapSqlParameterSource getMapSqlParameterSource(Meal meal, int userId) {
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories())
+                .addValue("date_time", meal.getDateTime())
+                .addValue("user_id", userId);
+        return map;
+    }
+
+    @Override
+    public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM meals WHERE user_id=?  AND date_time >=  ? AND date_time < ? ORDER BY date_time DESC",
+                ROW_MAPPER, userId, startDateTime, endDateTime);
     }
 }

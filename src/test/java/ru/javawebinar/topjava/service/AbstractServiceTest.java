6fcff08f800adcfc85c5d50d1941dff05898a6b3
Igor Strelchenya <just.sicarius@gmail.com>
2021-11-18 12:57:37 +0300

    HW06: task 1.2

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,10 +1,14 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
@@ -21,9 +25,30 @@ import ru.javawebinar.topjava.TimingRules;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 public abstract class AbstractServiceTest {
+
+    @Autowired
+    private Environment env;
+
     @ClassRule
     public static ExternalResource summary = TimingRules.SUMMARY;
 
     @Rule
     public Stopwatch stopwatch = TimingRules.STOPWATCH;
+
+    @Before
+    public void checkJdbc() {
+        Assume.assumeTrue(isJdbc());
+    }
+
+    private boolean isJdbc() {
+        String[] activeProfiles = env.getActiveProfiles();
+        if (activeProfiles.length > 0) {
+            for (String profile : activeProfiles) {
+                if (profile.equalsIgnoreCase("jdbc")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
